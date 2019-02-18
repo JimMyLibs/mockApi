218518feccaf9c6b606a32306ae81a79c001bd49
jim <lvjiangmin@niiwoo.com>
2019-02-18 18:15:10 +0800

    分页相关字段命名说明：totalNum,curPage,pageSize。 超出页码返回空数组处理。 totalNum,curPage,pageSize可选。

--- app/rules/jim/req_res/README.md
+++ app/rules/jim/req_res/README.md
@@ -1,5 +1,31 @@
 ## 本条规则使用说明
 
+> 分页相关字段命名说明：totalNum,curPage,pageSize
+```
+// 直接使用totalNum,curPage,pageSize字段
+"pageSize":10,
+"totalNum":23,
+"curPage":3
+```
+```
+// 自定义totalNum,curPage,pageSize字段
+"pageSize": {
+    "pageSizeName": 10
+},
+"totalNum": {
+    "totalNumName": 23
+},
+"curPage": {
+    "curPageName": 3
+}
+```
+> 分页相关字段默认值：
+```
+totalNum: 0-100随机,
+curPage: 1,
+pageSize: 10
+```
+
 > 通过请求字段，决定响应字段，不需编写接口
 ```
 - 示例1
--- app/rules/jim/req_res/rule.js
+++ app/rules/jim/req_res/rule.js
@@ -7,18 +7,18 @@ const Random = Mock.Random;
 const example = {
     getData: (res, data = {}, args = { ret: '10000', success: '' }) => {
         if (args.ret == '10000') {
-            const msg = Mock.mock({
+            const msg = {
                 ret: args.ret,
                 message: args.success ? ('模拟数据：' + args.success) : '模拟数据',
                 data,
-            })
+            }
             res.send(msg)
         } else {
-            const msg = Mock.mock({
+            const msg = {
                 ret: args.ret,
                 message: getErrMsg(args.ret),
                 data,
-            })
+            }
             res.send(msg)
         }
     },
@@ -31,17 +31,26 @@ const isArray = function (arr) {
 const isObject = function (obj) {
     return obj.constructor == Object;
 }
-const getRandom = (type, totalNum, curPage = 1, pageSize = 10) => {// 生成模拟数据
-    console.log('getRandom______', type, totalNum)
-    if (totalNum) {// 数组类型模拟
-        if(curPage == parseInt(totalNum / pageSize)){
-            return getMore(type).slice(0, parseInt(totalNum % pageSize), parseInt(totalNum % pageSize))
+const getArrRandom = (type, totalNum, curPage, pageSize) => {// 生成模拟数据: 数组类型模拟
+    console.log('getArrRandom______', type, totalNum)
+    if(totalNum && pageSize){
+        if(curPage == parseInt(totalNum / pageSize) + 1){
+            return getMore(type).slice(0, parseInt(totalNum % pageSize))
+        }else if(curPage > parseInt(totalNum / pageSize) + 1){
+            return getMore(type).slice(0, 0)
         }else{
             return getMore(type).slice(0, pageSize, pageSize)
         }
-    } else {// 数值类型模拟: 待处理
-        return Random[type]()
+    }else{
+        totalNum = totalNum || Random.integer(0,100);
+        if(!pageSize){
+            return getMore(type).slice(0, totalNum)
+        }
     }
+}
+const getRandom = (type) => {// 生成模拟数据: 数值类型模拟: 待处理
+    console.log('getRandom______', type)
+    return Random[type]()
 }
 const loopArr = (arr, totalNum, curPage, pageSize) => {// 二次数组递归
     let item = arr[0];    
@@ -53,25 +62,37 @@ const loopArr = (arr, totalNum, curPage, pageSize) => {// 二次数组递归
         return loopArr(item, totalNum, curPage, pageSize, true)
     } else {
         console.log('进来了2', item)// 狸猫换太子，否则就变成二维数组了
-        return Random[item] ? getRandom(item, totalNum, curPage, pageSize) : item
+        return Random[item] ? getArrRandom(item, totalNum, curPage, pageSize) : [item]
     }
 }
-const pageAttrPre = (arr) => {// totalNum/curPage/pageSize 在数组中前置: 待处理
-
+const pageAttrPre = (arr) => {// totalNum,curPage,pageSize 在数组中前置: 待处理
+    ['totalNum','curPage','pageSize'].map(item=>{
+        if(arr.includes(item)){
+            const curIndex = arr.indexOf(item);
+            arr.splice(curIndex,1)
+            arr.unshift(item);
+        }
+    }) 
+    return arr;
 }
-const loop = (args, totalNum = 0, curPage = 1, pageSize = 0, isList = false) => {// 一次递归
-    Object.keys(args).map(item => {
+const loop = (args, totalNum, curPage, pageSize, isInList = false) => {// 一次递归
+    const argsKeys = pageAttrPre(Object.keys(args));
+    const hasArr = argsKeys.some(item => isArray(args[item]));// 字段中存在数组
+    argsKeys.map(item => {
         if (isObject(args[item])) {// 对象
-            console.log('对象1', item)
-            if (item == 'totalNum') {// 总条数
-                totalNum = args[Object.entries(args[item])[0][0]] = Object.entries(args[item])[0][1];
-                delete args[item];
-            } else if (item == 'curPage') {// 总页码
-                curPage = args[Object.entries(args[item])[0][0]] = Object.entries(args[item])[0][1];
-                delete args[item];
-            } else if (item == 'pageSize') {// 总页码
-                pageSize = args[Object.entries(args[item])[0][0]] = Object.entries(args[item])[0][1];
-                delete args[item];
+            console.log('对象1', item);
+            if(hasArr){
+                // 自定义totalNum,curPage,pageSize字段
+                if (item == 'totalNum') {// 总条数
+                    totalNum = args[Object.entries(args[item])[0][0]] = Object.entries(args[item])[0][1];
+                    delete args[item];
+                } else if (item == 'curPage') {// 总页码
+                    curPage = args[Object.entries(args[item])[0][0]] = Object.entries(args[item])[0][1];
+                    delete args[item];
+                } else if (item == 'pageSize') {// 总页码
+                    pageSize = args[Object.entries(args[item])[0][0]] = Object.entries(args[item])[0][1];
+                    delete args[item];
+                }
             }  else {
                 loop(args[item])
             }
@@ -79,10 +100,10 @@ const loop = (args, totalNum = 0, curPage = 1, pageSize = 0, isList = false) => 
             console.log('数组1', item)
             args[item] = loopArr(args[item], totalNum, curPage, pageSize)
         } else {
-            console.log('进来了1', args, item, isList ? '是' : '否', '从数组中来')
-            if (isList) {// 数组里面的项目
+            console.log('进来了1', args, item, isInList ? '是' : '否', '从数组中来')
+            if (isInList) {// 数组里面的项目
                 if(Random[args[item]]){// 支持此语法
-                    args = getRandom(args[item], totalNum, curPage, pageSize).map(cell=>{
+                    args = getArrRandom(args[item], totalNum, curPage, pageSize).map(cell=>{
                         let obj = {};
                         obj[args[item]] = cell;
                         return obj;
@@ -90,8 +111,17 @@ const loop = (args, totalNum = 0, curPage = 1, pageSize = 0, isList = false) => 
                 }else{
                     args = args[item]
                 }
-            } else {
-                args[item] = Random[args[item]] ? getRandom(args[item]) : args[item]
+            } else {              
+                // totalNum,curPage,pageSize字段未自定义
+                if (item == 'totalNum') {// 总条数
+                    totalNum = args[item];
+                } else if (item == 'curPage') {// 总页码
+                    curPage = args[item];
+                } else if (item == 'pageSize') {// 总页码
+                    pageSize = args[item];
+                }else{
+                    args[item] = Random[args[item]] ? getRandom(args[item]) : args[item];
+                }
             }
         }
     })
